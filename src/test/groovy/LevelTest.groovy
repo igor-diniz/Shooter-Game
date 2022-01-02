86af594a534651efb26e905850feb4b3f8a0b0d6
ianitalo <ianitalo56@hotmail.com>
2022-01-02 14:46:59 +0000

    level colision test

--- src/test/groovy/LevelTest.groovy
+++ src/test/groovy/LevelTest.groovy
@@ -1,14 +1,20 @@
 import com.googlecode.lanterna.input.KeyStroke
+import game.Bullet
 import game.Level
 import game.Player
 import game.Position
 import game.Wall
 import game.enemies.Dreg
 import game.enemies.Enemy
+import game.weapons.HandCannon
 import spock.lang.Specification
+
+
 
 class LevelTest extends Specification
 {
+
+
     def 'Level Creation'()
     {
         given:
@@ -115,5 +121,40 @@ class LevelTest extends Specification
         then:
         level1.getEnemyList()[0].getPosition() == new Position(5,7)
         level1.getEnemyList()[1].getPosition() == new Position(8,6)
+    }
+
+    def 'collisions'()
+    {
+        Level level = new Level(10,10)
+        Player player = new Player(new Position(1,1))
+        Dreg dreg = new Dreg(new Position(8,8))
+        Dreg dreg2 = new Dreg(new Position(8,5))
+        List<Enemy> enemyList = new ArrayList<Enemy>()
+        enemyList.add(dreg)
+        enemyList.add(dreg2)
+        List<Wall> wallList = new ArrayList<Wall>()
+        for(int i = 0; i < level.getNumRows();i++)
+        {
+            wallList.add(new Wall(new Position(level.getNumRows()-1,i)))
+            wallList.add (new Wall(new Position(0, i)))
+        }
+        for(int i = 0; i < level.getNumColumns(); i++)
+        {
+            wallList.add(new Wall(new Position(i,0)))
+            wallList.add(new Wall(new Position(i,level.getNumColumns()-1)))
+        }
+        level.generateEntities(player,enemyList,wallList)
+        Bullet bullet1 = new Bullet(new Position(1,1),new HandCannon())
+        Bullet bullet2 = new Bullet(new Position(8,5),new HandCannon())
+        Bullet bullet3 = new Bullet(new Position(8,5),new HandCannon())
+        level.addBullet(bullet1)
+        level.addBullet(bullet2)
+        level.addBullet(bullet3)
+        when:
+        level.checkColisions()
+        then:
+        player.getHealth() == 2
+        level.getEnemyList().size() == 1
+        level.getBullets.size() == 1
     }
 }
