202e8f90510647ccc627e4ab1652a99904e5a2cd
Eduartico <duarte.eduardoeds@gmail.com>
2022-01-15 23:41:20 +0000

    Sniper Done + Created Machine Gun

--- src/main/java/game/weapons/MachineGun.java
+++ src/main/java/game/weapons/MachineGun.java
@@ -1,4 +1,16 @@
 package game.weapons;
 
-public class MachineGun {
+public class MachineGun extends Weapon{
+    @Override
+    protected int generateDamage() {
+        return 0;
+    }
+
+    @Override
+    protected int generateRange() {
+        return 0;
+    }
+
+    @Override
+    protected int getStartAmmo() {return 0;}
 }
