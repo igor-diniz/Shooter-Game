e2fafd30041582e7a58ee658aa44e00d423dc5da
ianitalo <ianitalo56@hotmail.com>
2022-01-20 01:05:36 +0000

    inventory state test

--- src/test/groovy/state/InventoryStateTest.groovy
+++ src/test/groovy/state/InventoryStateTest.groovy
@@ -11,5 +11,42 @@ import spock.lang.Specification
 
 class InventoryStateTest extends Specification
 {
+    private Game game;
+
+    void 'setup'()
+    {
+        game = new Game();
+    }
+
+    def 'Inventory Creation'()
+    {
+        when:
+        InventoryState inventoryState = new InventoryState(game)
+        then:
+        inventoryState.getSelected() == 0
+    }
+
+    def 'Inventory option selection'()
+    {
+        given:
+        InventoryState inventoryState = new InventoryState(game)
+        when:
+        inventoryState.previousOption()
+        inventoryState.nextOption()
+        inventoryState.nextOption()
+        then:
+        inventoryState.getSelected() == 3
+    }
+
+    def 'Inventory Show'()
+    {
+        given:
+        InventoryState inventoryState = new InventoryState(game)
+        GUI gui = Mock(GUI.class)
+        when:
+        inventoryState.show(gui)
+        then:
+        1 * gui.drawInventory(game,_,_)
+    }
 
 }
