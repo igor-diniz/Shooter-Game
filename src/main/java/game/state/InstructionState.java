cdcd19b69debccbe842f91be8cada96bae5fc64b
ianitalo <ianitalo56@hotmail.com>
2022-01-20 17:59:41 +0000

    instruction state implementation

--- src/main/java/game/state/InstructionState.java
+++ src/main/java/game/state/InstructionState.java
@@ -9,16 +9,16 @@ public class InstructionState implements State {
     private Game game;
     public InstructionState(Game game)
     {
-
+        this.game = game;
     }
 
     @Override
     public void show(GUI gui) {
-
+        gui.drawInstructions();
     }
 
     @Override
     public void processInput(KeyStroke keystroke) {
-
+        if(keystroke.getCharacter() == 'q' || keystroke.getCharacter() == 'Q'){ game.setState(new MenuState(game));}
     }
 }
